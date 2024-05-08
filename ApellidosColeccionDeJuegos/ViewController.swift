766392e2db6a8374176e58db5c87b7392b1966d7
LeonardoDíaz2003 <leonardo.diaz@tecsup.edu.pe>
2024-05-08 12:27:10 -0500

    Initial Commit

---
+++ ApellidosColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApellidosColeccionDeJuegos
+//
+//  Created by Leonardo Paolo Diaz Ocampo on 8/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
